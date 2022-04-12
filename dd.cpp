07dbc65c8cac2b100089dbf12b530de72a808a4f
Arisha75144 <88162494+Arisha75144@users.noreply.github.com>
2022-04-12 15:31:17 +0200

    Update dd.cpp

--- dd.cpp
+++ dd.cpp
@@ -4,91 +4,108 @@ using namespace std;
 #include <cmath>
 #include <algorithm>
 
-struct Point{
-  int x, y;
+struct Point {
+    int x, y;
 
-  Point(){
-    cin>>x>>y;
-  }
+    Point() {
+        cin >> x >> y;
+    }
 
-  double  calcDistance(Point& aOther){
-    return sqrt((aOther.x-x)*(aOther.x-x)+
-      (aOther.y-y)*(aOther.y-y));
-    
-  
-  }
-/*istream& operator>> (istream& is, Point& dt)
-{
-    is>> dt.x>> dt.y;
-    return is;
-}*/
-  Point(int aX, int aY){
-    x = aX; y = aY;
-  }
-bool operator<(Point& aOther){
-  if (x  != aOther.x) return x < aOther.x;
-  return y < aOther.y;
-  
-  
-}
-  void print(){
-    cout<<x<<" "<<y<<"\n";
-  }
+    double  calcDistance(Point& aOther) {
+        return sqrt((aOther.x - x)*(aOther.x - x) +
+            (aOther.y - y)*(aOther.y - y));
+    }
+
+    Point(int aX, int aY) {
+        x = aX; y = aY;
+    }
+    bool operator<(Point& aOther) {
+        if (x != aOther.x) return x < aOther.x;
+        return y < aOther.y;
+    }
+    void print() {
+        cout << x << " " << y << "\n";
+    }
 };
-struct  Square{
-vector<Point>points;
-  Square(){
-    points.resize(4);
-    int a=points[0].calcDistance(points[1]);
-    for (int i=0;i<points.size()-1;i++)
-      if(points[i].calcDistance(points[i+1])!=a)
-      {
-        cout<<"ne kv";
-        return;
+struct Rectangle {
+    vector<Point>points;
+
+    Rectangle() {
+        vector<int>a;
+        points.resize(4);
+        for (int i = 0; i < points.size(); i++) {
+            a.push_back(points[i].calcDistance(points[i + 1]));
         }
-        if (points[0].calcDistance(points.back())!=a)
-        cout<<"ne kv";
-    
-  }
- double  calcPerimetr(){
-   int suma=0;
-    for (int i=0;i<points.size()-1;i++)
-  suma+=points[i].calcDistance(points[i+1]);
-  return suma+points[0].calcDistance(points.back());
-   
-   
- }
-double calcArea(){
-  int  a=(points[0].calcDistance(points[1]));
-  return a*a;
+        for (int i = 0; i < a.size()-2; i++) {
+            if (a[i] != a[i + 2]) {
+              cout<<"\n";
+              cout<<i<<" ";
+              cout<<a[i]<<" "<<a[i+2]<<"\n";
+                cout << "ne rectangle"<<"\n";
+                break;
+            }
+
+        }
+    }
+
+    double calcArea() {
+        int  a = (points[0].calcDistance(points[1]));
+        int b = (points[1].calcDistance(points[2]));
+        return a * b;
+    }
+
+    double  calcPerimetr() {
+        int suma = 0;
+        for (int i = 0; i < points.size() - 1; i++)
+            suma += points[i].calcDistance(points[i + 1]);
+        return suma + points[0].calcDistance(points.back());
+    }
+virtual
+string proverka(){
+  return "rectangle"; 
+
   
 }
+};
 
+struct  Square:public Rectangle
+{
+    Square() {
+        int a = points[0].calcDistance(points[1]);
+        for (int i = 0; i < points.size() - 1; i++)
+            if (points[i].calcDistance(points[i + 1]) != a)
+            {
+                cout << "ne kv";
+                return;
+            }
+        if (points[0].calcDistance(points.back()) != a)
+            cout << "ne kv";
+
+    }
+string proverka(){
+  return "square"; }
 };
 
 int main() {
-  //int n;
-  //cin>>n;
-  int suma=0;
-  // vector  <Point>a (n);
-  // for (int i=0;i<a.size()-1;i++)
-  //   cout<<a[i].calcDistance(a[i+1])<<"\n";
-  // vector <Point>h(4);
-  // sort(h.begin(),h.end());
-  // for (int i=0;i<h.size();i++)
-  //   h[i].print();
+    Rectangle *d=new Square;
+  //Square fq;
+  cout<<d->proverka()<<"\n";
+  //cout<<fq.proverka();
   
-  //for (int i=0;i<h.size();i++)
-    //cin>>h[i].x >> h[i].y;
-  Square s;
-  
-  cout<<s.calcArea();
-  
-
-
-
-  
-  //p.create();
-  //p.print();
-
 }
+/*
+0 0
+0 5
+3 5
+3 0
+
+
+0 0
+0 7
+7 7
+7 0
+
+
+
+
+*/
